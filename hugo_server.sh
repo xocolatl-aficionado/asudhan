45794ec9a1a3a67f47edb7ee167fddd7dfd02cc6
Adithya.Sudhan <adithyasudhan@gmail.com>
2024-08-22 22:50:07 +0400

    add initial draft

---
+++ hugo_server.sh
@@ -0,0 +1,4 @@
+function hugo_server() {
+        WSL2_IPADDRESS=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+        hugo server --bind $WSL2_IPADDRESS --baseURL=http://$WSL2_IPADDRESS -D -F --gc -w
+}
