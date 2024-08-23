300b2c19eb77f2f4c96622aad21db698d76c8e1a
Adithya <adithyasudhan@gmail.com>
2024-08-23 16:24:10 +0400

    Make message more candid. Add a Projects section

--- hugo_server.sh
+++ hugo_server.sh
@@ -1,4 +1,4 @@
 function hugo_server() {
         WSL2_IPADDRESS=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-        hugo server --bind $WSL2_IPADDRESS --baseURL=http://$WSL2_IPADDRESS -D -F --gc -w
+        hugo server --bind $WSL2_IPADDRESS --baseURL=http://$WSL2_IPADDRESS -D -F --gc -w --disableFastRender --poll=1000ms
 }
